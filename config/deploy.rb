261faafa39a26ea8217dfe49e294a084c98283e3
harishjavali <harishjavaligp@gmail.com>
2024-03-12 00:11:48 +0530

    rename config cred file name

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :bundle_install_flags, '--verbose --no-deployment'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/application.yml'
+append :linked_files, "config/database.yml", 'config/credentials.yml.enc'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "public/uploads"
