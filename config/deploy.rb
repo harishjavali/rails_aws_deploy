15d4701007bc0b96a8bd2000d5b12ecaa82efb0a
harishjavali <harishjavaligp@gmail.com>
2024-03-12 00:53:53 +0530

    master key bug fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :bundle_install_flags, '--verbose --no-deployment'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/credentials.yml.enc', 'master.key'
+append :linked_files, "config/database.yml", 'config/credentials.yml.enc', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "public/uploads"
